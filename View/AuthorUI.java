1610421318c3771f6492f96038ae32878a56d76d
Enkhamgalan Baterdene <zak.etc@gmail.com>
2016-05-06 15:02:40 -0700

    Added Author and Reviewer UIs.

--- View/AuthorUI.java
+++ View/AuthorUI.java
@@ -5,10 +5,66 @@ package view;
 
 /**
  * 
- * @author
- * @version
+ * @author Enkhamgalan Baterdene
+ * @version v1.0
  *
  */
 public class AuthorUI {
 
+	Author myAuthor;
+	Conference myCurrentConference;
+	public AuthorUI () {
+		
+	}
+	
+	public void authorMenu() {
+    	int choice = -1;
+
+    	do {
+    		displayScreenHeader(String menuTitle)
+    		
+    		writeln("1) Submit a Manuscript");
+    		writeln("2) Modify/Remove a Manuscript");
+    		writeln("3) View a manuscript review");
+    		writeln("4) Back");
+    		writeln("5) Exit");
+    		writeln("\n > ");
+
+    		choice = myScanner.nextInt();
+            myScanner.nextLine();
+            
+    		switch(choice) {
+    			case 1:
+    				submitManuscriptMenu();
+    				break;
+    			case 2:
+    				modifyRemoveManuscript();
+    				break;
+    			case 3:
+    				viewManuscriptReview();
+    				break;
+    			case 4:
+    				// Call previous menu.
+    				break;
+    			default:
+    				// Exit from the menu.
+    				break;
+    		}
+
+    	} while(choice != 0);
+	}
+	
+	public void submitManuscriptMenu() {
+		
+	}
+	
+	public void modifyRemoveManuscript(){
+		
+	}
+	
+	public void viewManuscriptReview() {
+		
+	}
+	
+	
 }
--- View/ReviewerUI.java
+++ View/ReviewerUI.java
@@ -10,5 +10,76 @@ package view;
  *
  */
 public class ReviewerUI {
+	
+	Author myAuthor;
+	Conference myCurrentConference;
+	
+	public ReviewerUI () {
+		Author myAuthor = new Author();
+		Conference myCurrentConference = new Conference();
+	}
+	
+	/**
+     * Provides menu options for all Reviewer Actions.
+     */
+    private void reviewerMenu() {
+        //Submit a review
+    	int choice = -1;
 
+    	do {
+    		displayScreenHeader(String menuTitle)
+    		
+    		writeln("1) Download all Manuscripts");
+    		writeln("2) Update/Remove a Review");
+    		writeln("3) Upload Review");
+    		writeln("4) Back");
+    		writeln("5) Exit");
+    		writeln("\n > ");
+
+    		choice = myScanner.nextInt();
+            myScanner.nextLine();
+            
+    		switch(choice) {
+    			case 1:
+    				reviewerDownloadAllManuscripts();
+    				break;
+    			case 2:
+    				reviewerUpdateRemoveReview();
+    				break;
+    			case 3:
+    				reviewerUploadReview();
+    				break;
+    			case 4:
+    				// Call previous menu.
+    				break;
+    			default:
+    				// Exit from the menu.
+    				break;
+    		}
+
+    	} while(choice != 0);
+    }
+
+    
+	public boolean reviewerDownloadAllManuscripts() {
+		
+	}
+	
+	public boolean reviewerUpdateRemoveReview() {
+		
+	}
+	
+	public boolean reviewerUploadReview() {
+		
+	}
+	
+	
+	private void displayScreenHeader(String menuTitle) {
+        System.out.println(SystemHelper.SYS_TITLE);
+        System.out.println(myCurrentConference.getConferenceName());
+        System.out.println("Reviewer: " + myReviewer.getUserName());
+        System.out.println(menuTitle);
+    }
+	
 }
+
