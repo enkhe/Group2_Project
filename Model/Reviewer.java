f705f5a7a3a3090dc966595f2f88276bd287b3b2
AmritP <a_puri@outlook.com>
2016-05-08 09:29:40 -0700

    Populated PC, SPC, and Reviewer methods.

--- Model/Reviewer.java
+++ Model/Reviewer.java
@@ -1,33 +1,49 @@
 package model;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+
 /*
  * Author: Amrit Puri
  * Group 2 - TCSS 360A
  */
 
-public class Reviewer  extends RegisteredUser {
+public class Reviewer extends RegisteredUser implements Serializable {
 
-    private List<Manuscript> myManuscripts;
-
-    /*
-
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<Manuscript> myManuscripts;
+    
+    
+    /**
+		Default constructor
      */
-    public Reviewer(RegisteredUser theUser) {
-
+    public Reviewer () {
+		myManuscripts = new ArrayList<Manuscript>();
     }
 
-    /*
+    /**
+		Overloaded Constructor
+     */
+    public Reviewer(RegisteredUser theUser) {
+    	super(theUser.getFirstName(), theUser.getLastName(), theUser.getUserName(), theUser.getID());
+		myManuscripts = new ArrayList<Manuscript>();
+    }
 
+    /**
+	 * Returns a list of all Manuscripts the Reviewer is assigned.
      */
     public List<Manuscript> getMyAssignedManuscripts() {
         return myManuscripts;
     }
 
-    /*
+    /**
 
      */
     public void submitReview(int theScore, String theReviewPath) {
-
+    	
     }
 }
