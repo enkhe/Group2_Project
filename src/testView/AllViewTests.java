fa3c8e4d669ba918b37697a73e01a474fa3aea85
Shaun Coleman <shaunpcole@gmail.com>
2016-05-25 08:59:23 -0700

    added AllModelTests and AllVeiwTests suites

---
+++ src/testView/AllViewTests.java
@@ -0,0 +1,13 @@
+package testView;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AuthorUITest.class, ManagementSystemTest.class,
+		ProgramChairUITest.class, ReviewerTest.class,
+		SubProgramChairUITest.class })
+public class AllViewTests {
+
+}
