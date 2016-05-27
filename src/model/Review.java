363d148df11f79a42176bda65a453e3b82d4e507
tylerkb2 <get2tylerb@gmail.com>
2016-05-27 10:38:42 -0700

    Updated java doc comments.

--- src/model/Review.java
+++ src/model/Review.java
@@ -2,69 +2,62 @@ package model;
 import java.io.Serializable;
 
 /*
+ * Author: Tyler Brent
  * Group 2 - TCSS260A
+ */
+
+/**
+ * 
+ * This class represents a Review for a Manuscript. It contains the score and filepath
+ * to the review file.
+ *
  */
 
 public class Review implements Serializable{
 	
 	private static final long serialVersionUID = 6477667439086991275L;
-	/** Review score */
+	/** Review score. */
 	private int myScore;
-	/** File path for the review */
+	/** File path for the review file. */
 	private String myFilePath;
 	
-	/**
-	 * Default constructor.
-	 * myScore set to -1 to represent no score given.
-	 * myFilePath set to null to represent no review given.
-	 */
 	public Review() {
 		myScore = -1;
 		myFilePath = null;
 	}
 	
-	/**
-	 * Overloaded constructor.
-	 * 
-	 * @param theScore given by the reviewer.
-	 * @param theReview file path.
-	 */
 	public Review(int theScore, String theReview) {
 		myScore = theScore;
 		myFilePath = theReview;
 	}
 	
 	/**
-	 * Gets the score given by the reviewer.
-	 * 
-	 * @return the score.
+	 * Precondition: None.
+	 * Postcondition: Returns the review score.
 	 */
 	public int getScore() {
 		return myScore;
 	}
 	
 	/**
-	 * Gets the file path of the review file.
-	 * 
-	 * @return the file path.
+	 * Precondition: None.
+	 * Postcondition: Returns the file path to the review file.
 	 */
 	public String getReviewFile() {
 		return myFilePath;
 	}
 	
 	/**
-	 * Sets the review score.
-	 * 
-	 * @param theScore given by the reviewer.
+	 * Precondition: Takes an int as a review score.
+	 * Postcondition: Sets the review score to the one provided.
 	 */
 	public void setScore(int theScore) {
 		myScore = theScore;
 	}
 	
 	/**
-	 * Sets the file path to the review.
-	 * 
-	 * @param theFilePath of the review.
+	 * Precondition: Takes a String as a file path the the review file.
+	 * Postcondition: Sets the file path of the review file to the one provided.
 	 */
 	public void setReviewFile(String theFilePath) {
 		myFilePath = theFilePath;
