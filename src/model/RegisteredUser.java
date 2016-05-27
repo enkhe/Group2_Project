38a7b81832d8e7ceb6dd554af929824488ade711
tylerkb2 <get2tylerb@gmail.com>
2016-05-27 10:43:58 -0700

    Updated java doc comments

--- src/model/RegisteredUser.java
+++ src/model/RegisteredUser.java
@@ -6,6 +6,13 @@ package model;
 
 import java.io.Serializable;
 
+/**
+ * 
+ * This class represents a Registered User. It contains all identifying properties
+ * of anyone who has been registered into the system.
+ *
+ */
+
 public class RegisteredUser implements Serializable {
 	
 	private static final long serialVersionUID = 7108419094297515234L;
@@ -28,18 +35,34 @@ public class RegisteredUser implements Serializable {
 		myID = theID;
 	}
 	
+	/**
+	 * Precondition: None.
+	 * Postcondition: Returns the an int that represents the ID of the user.
+	 */
 	public int getID() {
 		return myID;
 	}
 	
+	/**
+	 * Precondition: None.
+	 * Postcondition: Returns the first name of the user.
+	 */
 	public String getFirstName() {
 		return myFirstName;
 	}
 	
+	/**
+	 * Precondition: None.
+	 * Postcondition: Returns the last name of the user.
+	 */
 	public String getLastName() {
 		return myLastName;
 	}
 	
+	/**
+	 * Precondition: None.
+	 * Postcondition: Returns the user name of the user.
+	 */
 	public String getUserName() {
 		return myUserName;
 	}
