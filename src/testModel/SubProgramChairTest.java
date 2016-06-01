41e05dbdd14522ea0bdd5146ea135d3bffddd6c7
AmritP <a_puri@outlook.com>
2016-06-01 01:34:03 -0700

    Moved ProgramChairUITest Business Rule Testing to SubProgramChairTest.java

--- src/testModel/SubProgramChairTest.java
+++ src/testModel/SubProgramChairTest.java
@@ -3,11 +3,9 @@ package testModel;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import model.*;
-import view.*;
 
 
 /**
@@ -19,6 +17,11 @@ import view.*;
 public class SubProgramChairTest {
 	RegisteredUser user;
 	SubProgramChair chair;
+	SubProgramChair mySubPC;
+	SubProgramChair mySubPCAssignedOneManuscript;
+	SubProgramChair mySubPCAssignedTwoManuscripts;
+	SubProgramChair mySubPCAssignedThreeManuscripts;
+	SubProgramChair mySubPCAssignedFourManuscripts;
 	Manuscript manuscript;
 	Manuscript manuscript2;
 	
@@ -28,21 +31,69 @@ public class SubProgramChairTest {
 		chair = new SubProgramChair(user);
 		manuscript = new Manuscript();
 		manuscript2 = new Manuscript();
+		chair.assignManuscript(manuscript);
+		chair.assignManuscript(manuscript2);
+		chair.unassignManuscript(manuscript);
+
+		mySubPC = new SubProgramChair(new RegisteredUser("Test", "Test", "Test", 123));
+		
+		mySubPCAssignedOneManuscript = new SubProgramChair();
+		mySubPCAssignedOneManuscript.assignManuscript(new Manuscript());
+		
+		mySubPCAssignedTwoManuscripts = new SubProgramChair();
+		mySubPCAssignedTwoManuscripts.assignManuscript(new Manuscript());
+		mySubPCAssignedTwoManuscripts.assignManuscript(new Manuscript());
+		
+		mySubPCAssignedThreeManuscripts = new SubProgramChair();
+		mySubPCAssignedThreeManuscripts.assignManuscript(new Manuscript());
+		mySubPCAssignedThreeManuscripts.assignManuscript(new Manuscript());
+		mySubPCAssignedThreeManuscripts.assignManuscript(new Manuscript());
+		
+		mySubPCAssignedFourManuscripts = new SubProgramChair();
+		mySubPCAssignedFourManuscripts.assignManuscript(new Manuscript());
+		mySubPCAssignedFourManuscripts.assignManuscript(new Manuscript());
+		mySubPCAssignedFourManuscripts.assignManuscript(new Manuscript());
+		mySubPCAssignedFourManuscripts.assignManuscript(new Manuscript());
 	}
 	
 	
 	@Test
 	public void testAssignManuscript() {
-		chair.assignManuscript(manuscript);
 		assertNotNull(chair.getMyAssignedManuscripts().get(0));
 	}
 
 	@Test
 	public void testUnassignManuscript() {
-		chair.assignManuscript(manuscript);
-		chair.assignManuscript(manuscript2);
-		chair.unassignManuscript(manuscript);
-		
 		assertFalse(chair.getMyAssignedManuscripts().contains(manuscript));
+	}
+	
+	@Test
+	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithNoAssignments() {
+		assertTrue(mySubPC.brCheck_SubprogramChairNotOverAssigned());
+	}
+
+	@Test
+	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithOneAssignments() {
+		assertTrue(mySubPCAssignedOneManuscript.brCheck_SubprogramChairNotOverAssigned());
+	}
+	
+	@Test
+	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithTwoAssignments() {
+		assertTrue(mySubPCAssignedTwoManuscripts.brCheck_SubprogramChairNotOverAssigned());
+	}
+	
+	@Test
+	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithThreeAssignments() {
+		assertTrue(mySubPCAssignedThreeManuscripts.brCheck_SubprogramChairNotOverAssigned());
+	}
+	
+	@Test
+	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithFourAssignments() {
+		assertTrue(mySubPCAssignedFourManuscripts.brCheck_SubprogramChairNotOverAssigned());
+	}
+	
+	@Test
+	public void testBrCheck_SubprogramChairCannotAssignOnUserWithFourAssignments() {
+		assertFalse(mySubPCAssignedFourManuscripts.assignManuscript(new Manuscript()) == 0);
 	}
 }
--- src/testView/ProgramChairUITest.java
+++ src/testView/ProgramChairUITest.java
@@ -13,10 +13,6 @@ import view.ProgramChairUI;
 
 public class ProgramChairUITest {
 	SubProgramChair mySubPC;
-	SubProgramChair mySubPCAssignedOneManuscript;
-	SubProgramChair mySubPCAssignedTwoManuscripts;
-	SubProgramChair mySubPCAssignedThreeManuscripts;
-	SubProgramChair mySubPCAssignedFourManuscripts;
 	Conference myConference;
 	Manuscript myManuscriptUserNotAuthor;
 	Manuscript myManuscriptUserIsAuthor;
@@ -30,24 +26,6 @@ public class ProgramChairUITest {
 		myConference = new Conference();
 		myUI = new ProgramChairUI(new RegisteredUser("Test", "Test", "Test", 123),
 				                  myConference);
-		
-		mySubPCAssignedOneManuscript = new SubProgramChair();
-		mySubPCAssignedOneManuscript.assignManuscript(new Manuscript());
-		
-		mySubPCAssignedTwoManuscripts = new SubProgramChair();
-		mySubPCAssignedTwoManuscripts.assignManuscript(new Manuscript());
-		mySubPCAssignedTwoManuscripts.assignManuscript(new Manuscript());
-		
-		mySubPCAssignedThreeManuscripts = new SubProgramChair();
-		mySubPCAssignedThreeManuscripts.assignManuscript(new Manuscript());
-		mySubPCAssignedThreeManuscripts.assignManuscript(new Manuscript());
-		mySubPCAssignedThreeManuscripts.assignManuscript(new Manuscript());
-		
-		mySubPCAssignedFourManuscripts = new SubProgramChair();
-		mySubPCAssignedFourManuscripts.assignManuscript(new Manuscript());
-		mySubPCAssignedFourManuscripts.assignManuscript(new Manuscript());
-		mySubPCAssignedFourManuscripts.assignManuscript(new Manuscript());
-		mySubPCAssignedFourManuscripts.assignManuscript(new Manuscript());
 	}
 
 	@Test
@@ -58,31 +36,6 @@ public class ProgramChairUITest {
 	@Test
 	public void testBrCheck_SubprogamNotAuthorOnUserIsAuthor() {
 		assertFalse(myUI.brCheck_SubprogamNotAuthor(myManuscriptUserIsAuthor, mySubPC));
-	}
-	
-	@Test
-	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithNoAssignments() {
-		assertTrue(myUI.brCheck_SubprogramChairNotOverAssigned(mySubPC));
-	}
-
-	@Test
-	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithOneAssignments() {
-		assertTrue(myUI.brCheck_SubprogramChairNotOverAssigned(mySubPCAssignedOneManuscript));
-	}
-	
-	@Test
-	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithTwoAssignments() {
-		assertTrue(myUI.brCheck_SubprogramChairNotOverAssigned(mySubPCAssignedTwoManuscripts));
-	}
-	
-	@Test
-	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithThreeAssignments() {
-		assertTrue(myUI.brCheck_SubprogramChairNotOverAssigned(mySubPCAssignedThreeManuscripts));
-	}
-	
-	@Test
-	public void testBrCheck_SubprogramChairNotOverAssignedOnUserWithFourAssignments() {
-		assertFalse(myUI.brCheck_SubprogramChairNotOverAssigned(mySubPCAssignedFourManuscripts));
 	}
 
 }
