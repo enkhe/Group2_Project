3ac3f7fc6a9a32c2b176950eccf05cd9bfd3ecc0
Tyler Brent <Tyler Brent>
2016-05-31 17:15:35 -0700

    Refactored ManuscriptTest.java

--- src/testModel/ManuscriptTest.java
+++ src/testModel/ManuscriptTest.java
@@ -1,80 +1,103 @@
-/**
 
- * Author: Tyler Brent
- * Group2 - TCSS360A
- */
 package testModel;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
 import model.*;
 
-/**
- * Test class for Manuscript.
- * Authored by: Tyler Brent
- */
 public class ManuscriptTest {
 	
 	Author author;
 	RegisteredUser user;
+	Review review;
 	Manuscript manuscript1;
 	Manuscript manuscript2;
 	Manuscript manuscript3;
+	File f;
+	File f2;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
+
 	@Before
 	public void setUp() throws Exception {
-		user = new RegisteredUser("Tyler", "Brent", "tylerkb2", 1234);
-		author = new Author(user);
+		//creating the user and author to submit from
+		user = new RegisteredUser("TEST_Tyler", "TEST_Brent", "TEST_tylerkb2", 1234);
+		author = new Author(user);	
+		
+		f = new File("TEST_manuscript.txt");
+		f.createNewFile();
+		f2 = new File("TEST_review.txt");
+		f2.createNewFile();
+		
+		//creating the manuscript from all constructors
 		manuscript1 = new Manuscript();
 		manuscript2 = new Manuscript(author.getID());
-		manuscript3 = new Manuscript(author.getID(), "filepath", "title");
+		manuscript3 = new Manuscript(author.getID(), f.getAbsolutePath(), "TEST_title");
+		
+		//submitting a manuscript to be reviewed
+		author.submitManuscript("TEST_Conference", manuscript3);
+		review = new Review(1111, manuscript3.getFile());
 	}
-
-	@Test(expected=IllegalArgumentException.class)
-	public void testSetRecommendation() throws Exception {
-		//test the bounds of recommendation input 1-5
+	
+	@Test
+	public void testSetReviewSuccess() {
+		//0 is returned for a successfully added and created review.
+		assertEquals(manuscript3.setReview(1111, review), 0);
+	}
+	
+	@Test
+	public void testSetReviewWithNullObject() {
+		//-1 is returned if the review is null and fails to set.
+		assertEquals(manuscript1.setReview(author.getID(), null), -1);
+	}
+	
+	@Test (expected = IOException.class)
+	public void testSetReviewWithIncorrectFilePath() {
+		//An incorrect path should throw an IOException and be caught
+		review.setReviewFile(review.getReviewFile() + "\\incorrectFilePath");
+		manuscript3.setReview(1111, review);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testSetRecommendationLessThanZero() {
+		//A value less than one should not be accepted and should throw an exception. 
 		manuscript1.setRecommendation(-1);
-		manuscript1.setRecommendation(0);
-		manuscript1.setRecommendation(1);
-		manuscript1.setAcceptStatus(2);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testSetRecommendationGreaterThanFour() {
+		//A value greater than four should not be accepted and should throw an exception.
 		manuscript1.setRecommendation(5);
-		manuscript1.setRecommendation(6);
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testSetAcceptStatus() throws Exception {
-		//test the bounds of accept status input 0-1
-		manuscript1.setAcceptStatus(-1);
-		manuscript1.setAcceptStatus(0);
-		manuscript1.setAcceptStatus(1);
-		manuscript1.setAcceptStatus(2);
 	}
 	
 	@Test
-	public void testGetRecommendation() {
-		//if no recommendation has been made then the default value should be -1
-		//which should return no recommendation.
-		assertEquals(manuscript2.getRecommendation(), "No Recommendation");
-		manuscript2.setRecommendation(1);
-		assertEquals(manuscript2.getRecommendation(), "Not Recommended");
-		manuscript2.setRecommendation(4);
-		assertEquals(manuscript2.getRecommendation(), "Strongly Recommended");
+	public void testSetRecommendationValueBetweenZeroAndFour() {
+		//A value between 0 and 4 should be accepted and properly set and return an
+		//appropriate string according to the scale in Manuscript. 3 = Recommended.
+		manuscript2.setRecommendation(3);
+		assertEquals(manuscript2.getRecommendation(), "Recommended");
 	}
 	
 	@Test
-	public void testGetAcceptStatus() {
-		//if no accept/reject decision has been made then the default value should
-		//be -1 which should return Pending.
-		assertEquals(manuscript3.getAcceptStatus(), "Pending");
-		manuscript3.setAcceptStatus(0);
-		assertEquals(manuscript3.getAcceptStatus(), "Rejected");
-		manuscript3.setAcceptStatus(1);
-		assertEquals(manuscript3.getAcceptStatus(), "Accepted");
+	public void testSetAcceptStatusZero() {
+		manuscript2.setAcceptStatus(0);
+		assertEquals(manuscript2.getAcceptStatus(), "Rejected");
 	}
-
+	
+	@Test
+	public void testSetAcceptStatusOne() {
+		manuscript2.setAcceptStatus(1);
+		assertEquals(manuscript2.getAcceptStatus(), "Accepted");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testSetAcceptStatusToNotZeroOrOne() {
+		//A value other than 1 or 0 should throw an exception if you try
+		//to set it.
+		manuscript2.setAcceptStatus(4);
+	}
 }
